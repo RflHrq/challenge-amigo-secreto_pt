e1a5a1432af1769dff459c65c407ceae0beb62d2
RflHrq <rflhrq@gmail.com>
2025-02-21 14:52:47 -0300

    Resolução do Projeto

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Código criado por Rafael Santos
+
+let nomes = [];
+
+function adicionarAmigo() {
+    let nome = document.getElementById('amigo').value.trim();
+    if (nome === '') {
+        alert('Digite um nome válido! (o nome não pode ser vazio)');
+        return;
+    }
+    nomes.push(nome);
+    document.getElementById('amigo').value = '';
+    atualizarAmigo();
+    document.getElementById('resultado').innerHTML = '';
+    console.log(nomes);
+}
+
+function atualizarAmigo() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = nomes.map(nome => `<li>${nome}</li>`).join('');
+
+}
+
+function sortearAmigo() {
+    if (nomes.length === 0) {
+        alert('Nenhum nome para sortear!');
+        return;
+    }
+    let sorteado = Math.floor(Math.random() * nomes.length);
+    document.getElementById('resultado').innerHTML = `Nome sorteado: ${nomes[sorteado]}`;
+    nomes = [];
+    atualizarAmigo();
+}
